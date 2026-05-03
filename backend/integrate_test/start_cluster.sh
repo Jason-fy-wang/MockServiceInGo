71f2d0654cd9d3b9f87ff82448f938ce73c98f2b
Jason-fy-wang <544094478@qq.com>
2026-05-03 10:35:21 +0000

    add integrte test folder

---
+++ backend/integrate_test/start_cluster.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# start cluster to do integrate test
+go build -o starter cmd/starter/starter.go
+
+
+./starter -config start1.json &
+./starter -config start2.json &
+./starter -config start3.json &
+
+
