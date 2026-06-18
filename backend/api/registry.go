f433a608b6e663d5dbce348f719252291d4108fd
Jason-fy-wang <544094478@qq.com>
2026-06-18 15:35:38 +0000

    update json field

--- backend/api/registry.go
+++ backend/api/registry.go
@@ -11,9 +11,9 @@ import (
 type ResponseType string
 
 const (
-	ResponseTypeHTTP      ResponseType = "http"
-	ResponseTypeSSE       ResponseType = "sse"
-	ResponseTypeWebSocket ResponseType = "websocket"
+	ResponseTypeHTTP      ResponseType = "HTTP"
+	ResponseTypeSSE       ResponseType = "SSE"
+	ResponseTypeWebSocket ResponseType = "WebSocket"
 )
 
 type SSEEvent struct {
@@ -31,13 +31,13 @@ type WebSocketMessage struct {
 type MockRule struct {
 	Method            string             `json:"method"`
 	Path              string             `json:"path"`
-	RequestHeaders    map[string]string  `json:"headers,omitempty"`
-	RequestBody       string             `json:"body,omitempty"`
-	RequestQuery      map[string]string  `json:"query,omitempty"`
-	ResponseStatus    int                `json:"status,omitempty"`
+	RequestHeaders    map[string]string  `json:"requestHeaders,omitempty"`
+	RequestBody       string             `json:"requestBody,omitempty"`
+	RequestQuery      map[string]string  `json:"requestQuery,omitempty"`
+	ResponseStatus    int                `json:"responseStatus,omitempty"`
 	ResponseHeaders   map[string]string  `json:"responseHeaders,omitempty"`
 	ResponseBody      string             `json:"responseBody,omitempty"`
-	ResponseType      ResponseType       `json:"responseType,omitempty"` // "http", "sse", "websocket"
+	ResponseType      ResponseType       `json:"responseType,omitempty"` // "HTTP", "SSE", "WebSocket"
 	SSEEvents         []SSEEvent         `json:"sseEvents,omitempty"`
 	WebSocketMessages []WebSocketMessage `json:"websocketMessages,omitempty"`
 }
---
+++ backend/integrate_test/start_standalone.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+script_dir=$(dirname "$0")
+script_name=$(basename "$0")
+
+cd "$script_dir" || exit 1
+
+# start cluster to do integrate test
+# go build -o starter ../cmd/starter/starter.go
+
+nohup ./starter -config start-standalone.json  >/dev/null 2>&1 &
